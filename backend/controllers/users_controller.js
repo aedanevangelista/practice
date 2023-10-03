16ce45a9bbe6536cc324b691046a4aea7b5c4731
jinshin19 <117880307+jinshin19@users.noreply.github.com>
2023-10-03 13:04:38 +0800

    Jinshin: createUser instance is created

--- backend/controllers/users_controller.js
+++ backend/controllers/users_controller.js
@@ -1,17 +1,64 @@
 const Users = require('../models/users_model')
 const { hash_password, compare_passwoord } = require('../util/password_helper')
 
-const createUser = ( request, response ) => {
+const createUser = async ( request, response ) => {
 
-    const { fullname, email, password } = request.body
+    const { fullname, email, confirm_password } = request.body
 
-    response.status(200).send(
+    if( !fullname, !email, !request.body.password ) return response.status(400).send(
         {
-            users: {
-                fullname,
-                email,
-                password
-            }
+            message: "Fields are required"
+        }
+    )
+
+    if( !fullname ) return response.status(400).send(
+        {
+            message: "Fullname is required"
+        }
+    )
+
+    if( !email ) return response.status(400).send(
+        {
+            message: "Email is required"
+        }
+    )
+
+    if( !request.body.password ) return response.status(400).send(
+        {
+            message: "Password is required"
+        }
+    )
+
+    if( !confirm_password ) return response.status(400).send(
+        {
+            message: "Password is required"
+        }
+    )
+
+    if( request.body.password !== confirm_password ) return response.status(400).send(
+        {
+            message: "Password is not matched"
+        }
+    )
+
+    const userDb = await Users.findOne( { email } )
+
+    if( userDb ) return response.status(409).send(
+        {
+            message: "This email is already registered"
+        }
+    )
+
+    const password = hash_password(request.body.password)
+    
+    const newUser = await Users.create( { fullname, email, password } )
+
+    newUser.save()
+
+    response.status(201).send(
+        {
+            status: 'OK',
+            message: "Registered Successfully"
         }
     )
 
